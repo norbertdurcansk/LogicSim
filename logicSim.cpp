d3cbb934c03894ba9e3d20b8eca28caab6be7f0a
norbertdurcansk <norbert.durcansky@gmail.com>
2015-11-07 22:33:29 +0100

    OR

--- logicSim.cpp
+++ logicSim.cpp
@@ -7,22 +7,20 @@
 #include <functional>
 
 using namespace std;
+#define MAX_UNIT 200 //MAX number of units in one circuit
+//=========================
 //main classes declaration 
-//Class Circuit --> defines whole logic circuit with all units
-//Class Unit --> defines one unit 
-//Class AND --> inherit Unit's att + 'AND' behavior
-//Class OR --> inherit Unit's att + 'OR' behavior
+//=========================
 
+// random number 0...1
 int Randomsig() {
     return int(2*Random()); //  0 .. 1
 }
 
-Facility Unit[200]; 
+// facility for each Unit 
+Facility Units[MAX_UNIT]; 
 
-
-
-
-
+// class implements  unit AND with two outputs and one input 
 class AND {
      public:
         int input1=-1;
@@ -38,6 +36,9 @@ class AND {
         void init(){ input1=-1;input2=-1;output=-1;}
         bool in(string pad){if(pad!=s_input1 && pad!=s_input2) return false; return true;}
 };
+
+
+// class implements  unit OR with two outputs and one input 
 class OR {
      public:
         int input1=-1;
@@ -54,8 +55,30 @@ class OR {
         bool in(string pad){if(pad!=s_input1 && pad!=s_input2) return false; return true;}
 };
 
+class Unit
+{
+public:
+    string type;
+    AND *And;
+    OR *Or;
+    Unit(string type,AND *And,OR *Or){this->type=type;this->And=And;this->Or=Or;};
+};
 
-vector <reference_wrapper<AND>> units;
+// vector with references on our circuit from file 
+vector <reference_wrapper<Unit>> units;
+
+class zapisOR : public Process{
+void Behavior();
+public:
+    OR *hradlo;
+    int value;
+    string input; 
+    int val;
+    zapisOR(OR *hradlo,int value,string input,int val){this->hradlo=hradlo;this->value=value;this->input=input;this->val=val;};
+};
+
+
+
 
 
 class zapisAND : public Process{
@@ -68,6 +91,13 @@ public:
     zapisAND(AND *hradlo,int value,string input,int val){this->hradlo=hradlo;this->value=value;this->input=input;this->val=val;};
 };
 
+
+
+
+
+
+
+
 class INSignal : public Process {
     public:
         string input; //name of the signal
@@ -79,16 +109,22 @@ class INSignal : public Process {
         void Behavior() {
             unsigned int val=0;  //for each  unit 
             for (;val<units.size();val++){
-                AND &l=units.at(val);
-                (new zapisAND(&l,value,input,val))->Activate();
+                Unit &l=units.at(val);
+                if((&l)->type=="AND")
+                (new zapisAND((&l)->And,value,input,val))->Activate();
+
+                else if((&l)->type=="OR")
+                (new zapisOR((&l)->Or,value,input,val))->Activate();
             }
     }
 
 };
 
+
+
  void zapisAND::Behavior() { 
         if(!hradlo->in(input)) return;
-        Seize(Unit[val]);
+        Seize(Units[val]);
         hradlo->setvalues(input,value);
 
         if(hradlo->outvalue()!=-1)    
@@ -105,8 +141,33 @@ class INSignal : public Process {
         {   
             Print("%i %i   %s guessed %f\n",val,hradlo->lastout,Time,input.c_str());
         }
-        Release(Unit[val]);
+        Release(Units[val]);
     }
+
+ void zapisOR::Behavior() { 
+        if(!hradlo->in(input)) return;
+        Seize(Units[val]);
+        hradlo->setvalues(input,value);
+
+        if(hradlo->outvalue()!=-1)    
+        {       
+                Wait(5);
+                Print("%i %i  %f\n",val,hradlo->output,Time);
+                hradlo->lastout=hradlo->output;
+                int  val1=hradlo->outvalue();
+                hradlo->init();  
+
+            (new INSignal(hradlo->soutvalue().c_str(),val1))->Activate();
+        }
+        else
+        {   
+            Print("%i %i   %s guessed OR %f\n",val,hradlo->lastout,Time,input.c_str());
+        }
+        Release(Units[val]);
+    }
+
+
+
 
 
 
@@ -122,19 +183,38 @@ private:
 };
 
 
+
+
+
 int main(){ // links for each unit 
     AND u;
     u.s_input1="IN1";
     u.s_input2="IN2";
      u.s_output="OUT";
-    AND &obj_ref1=std::ref(u);
+     Unit x("AND",&u,NULL);
+    Unit &obj_ref1=std::ref(x);
     units.push_back(obj_ref1);
-    AND s;
-    s.s_input1="OUT";
-    s.s_input2="OUT";
-     s.s_output="xx";
-    AND &objj_ref1=std::ref(s);
+   
+   AND z;
+    z.s_input1="IN1";
+    z.s_input2="OUT";
+     z.s_output="xx";
+     Unit v("AND",&z,NULL);
+    Unit &objj_ref1=std::ref(v);
     units.push_back(objj_ref1);
+
+   OR n;
+    n.s_input1="xx";
+    n.s_input2="xx";
+     n.s_output="ZZ";
+     Unit m("OR",NULL,&n);
+    Unit &objjj_ref1=std::ref(m);
+    units.push_back(objjj_ref1);
+
+
+
+
+   
 
   SetOutput("test.out");
   Print("id    value   time \n");
